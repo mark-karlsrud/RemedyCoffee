eeba7f0bc3c4f18a7b7c45c32574a7c4268ae235
Mark Karlsrud <markkarlsrud@gmail.com>
2018-03-14 22:23:59 -0400

    Initial Commit

---
+++ RemedyCoffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemedyCoffee
+//
+//  Created by Mark Karlsrud on 3/14/18.
+//  Copyright © 2018 Mark Karlsrud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
